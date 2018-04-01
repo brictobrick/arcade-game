e4ea6785ed4bf8b10d979022803a4275c5bce7db
brictobrick <minchang.k@gmail.com>
2018-04-02 02:08:35 +0900

    feat: Render bugs and a player

--- js/app.js
+++ js/app.js
@@ -1,35 +1,64 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+  constructor(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
+    this.x = x;
+    this.y = y;
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    dt = 20;
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  // Draw the enemy on the screen, required method for game
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+  constructor(x,y) {
+    this.sprite = 'images/char-cat-girl.png';
+    this.x = x;
+    this.y = y;
+  }
+
+  update(dt) {
+    dt = 10;
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput() {
+
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+const enemy1 = new Enemy(1,1);
+const enemy2 = new Enemy(100,50);
+const enemy3 = new Enemy(300,150);
+const allEnemies = [enemy1, enemy2, enemy3];
 
+// Place the player object in a variable called player
+const player = new Player(200,430);
 
 
 // This listens for key presses and sends the keys to your
